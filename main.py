252ed9ba77815df4ee0f2374db1d083a1437154b
Tom Lab <tom.fourcaudot@gmail.com>
2026-06-02 15:47:06 +0900

    add dataset

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# fetch dataset
+iris = fetch_ucirepo(id=53)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# data (as pandas dataframes)
+x = iris.data.features
+y = iris.data.targets
