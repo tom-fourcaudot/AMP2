b10cbc99178b37b0f232da3028991e90b7d58ac6
Tom Lab <tom.fourcaudot@gmail.com>
2026-06-04 14:36:17 +0900

    refractor x times knn

--- knn.py
+++ knn.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from collections import Counter
 
+from numpy.ma.core import mean
+
+from utils.data_split import split_data
+
+
 def squared_euclidian_distance(a: pd.Series, b: pd.Series):
     return ((a['sepal length'] - b['sepal length'])**2
             + (a['sepal width'] - b['sepal width'])**2
@@ -17,3 +22,18 @@ def knn(df_train: pd.DataFrame, data_test: pd.Series, k: int = 3):
     neighbors_predictions = Counter(tup[1] for tup in k_neighbors)
     prediction = neighbors_predictions.most_common(1)
     return prediction[0][0], prediction[0][1]/k
+
+def x_times_knn(data: pd.DataFrame, k: int, n: int = 50):
+    accuracies = []
+    for _ in range(n):
+        training, testing = split_data(data)
+
+        result = []
+        for index, row in testing.iterrows():
+            prediction = knn(training, row, k)
+            result.append((prediction, row['class'], prediction[0] == row['class']))
+
+        corrects = sum(1 if d[2] else 0 for d in result)
+        accuracy = corrects/len(training)
+        accuracies.append(accuracy)
+    return mean(accuracies)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import pandas as pd
-from numpy.ma.core import mean
 from ucimlrepo import fetch_ucirepo
 
-from utils.data_split import split_data
-from knn import knn
+from knn import x_times_knn
 
 # fetch dataset
 iris = fetch_ucirepo(id=53)
@@ -13,26 +10,12 @@ x = iris.data.features
 y = iris.data.targets
 data = x.join(y, how='inner')
 
+# test knn
 try_K = [1, 2, 3, 5, 7, 10, 15]
-iterations = 50
+iterations = 20
 
 accuracies = {}
-
-
 for k in try_K:
-    tmp = []
-    for _ in range(iterations):
-        training, testing = split_data(data)
-
-        result = []
-        for index, row in testing.iterrows():
-            prediction = knn(training, row, k)
-            result.append((prediction, row['class'], prediction[0] == row['class']))
-
-        corrects = sum(1 if d[2] else 0 for d in result)
-        accuracy = corrects/len(training)
-        tmp.append(accuracy)
-    print(f'Average accuracy for K = {k} -> {mean(tmp)}')
-    accuracies[k] = mean(tmp)
+    accuracies[k] = x_times_knn(data, k=k, n=iterations)
 
 print(accuracies)
