396e09d055730496cab7e2e19d8e988e6f0c8caf
Tom Lab <tom.fourcaudot@gmail.com>
2026-06-04 14:41:03 +0900

    split data by class

--- utils/data_split.py
+++ utils/data_split.py
@@ -4,3 +4,7 @@ def split_data(df: pd.DataFrame, ratio: float = 0.5):
     training = df.sample(frac=ratio)
     test = df.drop(training.index)
     return training, test
+
+def by_class_separation(df: pd.DataFrame):
+    df1, df2, df3 = [x for _, x in df.groupby('class')]
+    return df1, df2, df3
