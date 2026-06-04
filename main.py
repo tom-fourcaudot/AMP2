e232c92392977ebfee859c15f256ed4a50a4c28a
Tom Mac <tom.fourcaudot@gmail.com>
2026-06-04 14:07:54 +0900

    Test KNN N times with different K

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from numpy.ma.core import mean
 from ucimlrepo import fetch_ucirepo
 
 from utils.data_split import split_data
+from knn import knn
 
 # fetch dataset
 iris = fetch_ucirepo(id=53)
@@ -11,7 +13,26 @@ x = iris.data.features
 y = iris.data.targets
 data = x.join(y, how='inner')
 
-training, testing = split_data(data)
+try_K = [1, 2, 3, 5, 7, 10, 15]
+iterations = 50
 
-print(training)
-print(testing)
+accuracies = {}
+
+
+for k in try_K:
+    tmp = []
+    for _ in range(iterations):
+        training, testing = split_data(data)
+
+        result = []
+        for index, row in testing.iterrows():
+            prediction = knn(training, row, k)
+            result.append((prediction, row['class'], prediction[0] == row['class']))
+
+        corrects = sum(1 if d[2] else 0 for d in result)
+        accuracy = corrects/len(training)
+        tmp.append(accuracy)
+    print(f'Average accuracy for K = {k} -> {mean(tmp)}')
+    accuracies[k] = mean(tmp)
+
+print(accuracies)
