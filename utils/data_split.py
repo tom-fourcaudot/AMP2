dfcc04a8943f7b0e8d7dfffc7cb9b42e236f7c73
Tom Mac <tom.fourcaudot@gmail.com>
2026-06-08 15:24:32 +0900

    Introduce proportional data split to reduce randomness.

--- utils/data_split.py
+++ utils/data_split.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def split_data(df: pd.DataFrame, ratio: float = 0.5) -> tuple[pd.DataFrame, pd.DataFrame]:
+def split_data_random(df: pd.DataFrame, ratio: float = 0.5) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
-    Split data into train and test
+    Split data into train and test. This slitting split randomly without taking care of classes.
     :param df: the row dataframe
     :param ratio: the ratio of train and test between 0 and 1
     :return: tuple of train and test dataframes
@@ -23,3 +23,21 @@ def by_class_separation(df: pd.DataFrame)  -> tuple[pd.DataFrame, pd.DataFrame, 
     """
     df1, df2, df3 = [x for _, x in df.groupby('class')]
     return df1, df2, df3
+
+def split_data_proportional(df: pd.DataFrame, ratio: float = 0.5) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split data into train and test. This slitting split ensure you have the same amount of samples of each class
+    :param df: the row dataframe
+    :param ratio: the ratio of train and test between 0 and 1
+    :return: tuple of train and test dataframes
+    """
+    df_by_class = by_class_separation(df)
+    trainings = []
+    testings = []
+    for class_df in df_by_class:
+        df_train, df_test = split_data_random(class_df, ratio)
+        trainings.append(df_train)
+        testings.append(df_test)
+    training = pd.concat(trainings)
+    testing = pd.concat(testings)
+    return training, testing
