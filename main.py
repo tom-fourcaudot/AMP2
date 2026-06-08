ac74cd02dffa43e8fbfd19c5ea0fa25f44964dbf
Tom Mac <tom.fourcaudot@gmail.com>
2026-06-08 15:30:10 +0900

    refractor main with all previous changes

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from ucimlrepo import fetch_ucirepo
 
 from fisher import get_projection_vector
 from knn import x_times_knn
-from utils.data_split import by_class_separation, split_data
-
-import numpy as np
+from utils.data_split import by_class_separation, split_data_proportional
 
 # fetch dataset
 iris = fetch_ucirepo(id=53)
@@ -28,7 +26,7 @@ def test_knn():
     print(accuracies)
 
 
-training, testing = split_data(data)
+training, testing = split_data_proportional(data)
 
 df1, df2, df3 = by_class_separation(training)
 class1 = df1['class'].unique()[0]
@@ -41,11 +39,20 @@ classifiers = {class1: get_projection_vector(df1, pd.concat([df2, df3])),
 
 print(classifiers)
 
+fisher_results = []
 for index, row in testing.iterrows():
     c = row['class']
     data = row.drop('class').to_numpy()
 
+    predictions = {}
     for model_class, classifier in classifiers.items():
-        projection_vector, threshold, order = classifier
+        projection_vector, threshold = classifier
         projection = data @ projection_vector
-        prediction = order[0] if projection < threshold else order[1]
+        predictions[model_class] = (projection - threshold)
+    p = max(predictions, key=predictions.get)
+    fisher_results.append((c, p, predictions[p]))
+print(fisher_results)
+good_predictions = sum(1 for ground_true, prediction, _  in fisher_results if ground_true == prediction)
+accuracy = good_predictions / len(fisher_results)
+print(f'Accuracy: {accuracy*100}%')
+
