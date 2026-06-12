9a4a2db08ed063cd0f1c8d7f42491d4f79be5093
Tom Mac <tom.fourcaudot@gmail.com>
2026-06-12 12:55:46 +0900

    test fisher function

--- fisher.py
+++ fisher.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from utils.data_split import split_data_proportional, by_class_separation
+
 
 def get_projection_vector(df1: pd.DataFrame, df2: pd.DataFrame) -> tuple[list[float], float]:
     """
@@ -34,3 +36,33 @@ def get_projection_vector(df1: pd.DataFrame, df2: pd.DataFrame) -> tuple[list[fl
         w = -w
         threshold = (mean1 @ w + mean2 @ w) / 2
     return w, threshold
+
+def fisher(data: pd.DataFrame):
+    training, testing = split_data_proportional(data)
+
+    df1, df2, df3 = by_class_separation(training)
+    class1 = df1['class'].unique()[0]
+    class2 = df2['class'].unique()[0]
+    class3 = df3['class'].unique()[0]
+
+    classifiers = {class1: get_projection_vector(df1, pd.concat([df2, df3])),
+                   class2: get_projection_vector(df2, pd.concat([df3, df1])),
+                   class3: get_projection_vector(df3, pd.concat([df1, df2]))}
+
+    fisher_results = []
+    for index, row in testing.iterrows():
+        c = row['class']
+        row_features = row.drop('class').to_numpy()
+
+        predictions: dict = {}
+        for model_class, classifier in classifiers.items():
+            projection_vector, threshold = classifier
+            projection = row_features @ projection_vector
+            predictions[model_class] = (projection - threshold)
+        p = max(predictions, key=lambda k: predictions[k])
+        fisher_results.append((c, p, predictions[p]))
+    return fisher_results
+
+
+    # good_predictions = sum(1 for ground_true, prediction, _ in fisher_results if ground_true == prediction)
+    # accuracy = good_predictions / len(fisher_results)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from statistics import mean
+
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
-from fisher import get_projection_vector
+from fisher import fisher
 from knn import x_times_knn
-from utils.data_split import by_class_separation, split_data_proportional
 
 # fetch dataset
 iris = fetch_ucirepo(id=53)
@@ -15,44 +16,24 @@ data:pd.DataFrame = x.join(y, how='inner')
 
 
 # test knn
-def test_knn():
+def test_knn(iterations: int = 20):
     try_k = [1, 2, 3, 5, 7, 10, 15]
-    iterations = 20
-
     accuracies = {}
     for k in try_k:
         accuracies[k] = x_times_knn(data, k=k, n=iterations)
 
     print(accuracies)
 
+def test_fisher(iterations: int = 20):
+    accuracies = []
+    for _ in range(iterations):
+        result = fisher(data)
+        good_predictions = sum(1 for ground_true, prediction, _ in result if ground_true == prediction)
+        accuracy = good_predictions / len(result)
+        accuracies.append(accuracy)
+    fisher_accuracy = mean(accuracies)
+    print(fisher_accuracy)
 
-training, testing = split_data_proportional(data)
-
-df1, df2, df3 = by_class_separation(training)
-class1 = df1['class'].unique()[0]
-class2 = df2['class'].unique()[0]
-class3 = df3['class'].unique()[0]
-
-classifiers = {class1: get_projection_vector(df1, pd.concat([df2, df3])),
-               class2: get_projection_vector(df2, pd.concat([df3, df1])),
-               class3: get_projection_vector(df3, pd.concat([df1, df2]))}
-
-print(classifiers)
-
-fisher_results = []
-for index, row in testing.iterrows():
-    c = row['class']
-    row_features = row.drop('class').to_numpy()
-
-    predictions: dict = {}
-    for model_class, classifier in classifiers.items():
-        projection_vector, threshold = classifier
-        projection = row_features @ projection_vector
-        predictions[model_class] = (projection - threshold)
-    p = max(predictions, key=lambda k: predictions[k])
-    fisher_results.append((c, p, predictions[p]))
-print(fisher_results)
-good_predictions = sum(1 for ground_true, prediction, _  in fisher_results if ground_true == prediction)
-accuracy = good_predictions / len(fisher_results)
-print(f'Accuracy: {accuracy*100}%')
-
+if __name__ == '__main__':
+    test_knn(iterations=20)
+    test_fisher(iterations=20)
